73dc96b98b1714bd15ed1adc79c00dc9a5cddcea
Rodrigo Chiossi <rodrigochiossi@gmail.com>
2011-06-29 10:26:31 -0300

    Removed old files

--- x86/base.c
+++ x86/base.c
@@ -3,6 +3,7 @@
 char shell[] = "/bin/sh";
 
 int main(void) {
+  setreuid(1001,1001);  
   execve(shell,NULL,NULL);
 }
 
--- x86/base2.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-
-char shell[] = "/bin/sh";
-
-int main(void) {
-  setreuid(1001,1001);  
-  execve(shell,NULL,NULL);
-}
-
